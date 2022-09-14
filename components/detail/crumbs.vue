1fd262a3e88520db7404ff33ae05aee5ea168784
hanyucd <1807003141@qq.com>
2022-09-14 22:41:35 +0800

    feat: 完成产品详情逻辑

---
+++ components/detail/crumbs.vue
@@ -0,0 +1,25 @@
+﻿<template>
+  <div class="m-crumbs">
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geoModule.position.city.replace('市','') }}美团</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geoModule.position.city.replace('市','') }}{{ type }}</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">{{ $store.state.geoModule.position.city.replace('市','') }}{{ decodeURIComponent(keyword) }}</a></el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailCrumb',
+  props: {
+    keyword: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
---
+++ components/detail/list.vue
@@ -0,0 +1,26 @@
+﻿<template lang="html">
+  <div class="m-detail-list">
+    <ul>
+      <li>{{ list.filter(item=>item.photos.length).length }}款套餐</li>
+      <item v-for="(item,idx) in list" :key="idx" :meta="item" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './item.vue';
+export default {
+  name: 'DetailList',
+  components: {
+    Item
+  },
+  props: {
+    list: {
+      type: Array,
+      default: () => {
+        return [];
+      }
+    }
+  }
+};
+</script>
